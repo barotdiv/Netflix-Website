3b3a523385e05c09e2b635a5a484b1459e04fdb2
barotdiv <divbarot15@gmail.com>
2026-07-02 16:23:15 +0530

    file modify

--- frontend/src/utils/firebase-config.js
+++ frontend/src/utils/firebase-config.js
@@ -1,9 +1,7 @@
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-
-}
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
