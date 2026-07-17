38b13ac9141c8f1969dce09ef4f1b8f28d8d8907
barotdiv <divbarot15@gmail.com>
2026-07-17 16:10:23 +0530

    implement user routes for managing liked movies

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -7,7 +7,7 @@ const {
 const router = require("express").Router();
 
 router.get("/liked/:email", getLikedMovies);
-router.post("/add", addToLikedMovies),
-    router.put("/remove", removeFromLikedMovies);
+router.post("/add", addToLikedMovies);
+router.put("/remove", removeFromLikedMovies);
 
 module.exports = router;
