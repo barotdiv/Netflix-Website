91393885e8c7dd4f32416a0ed3ff5dd583e8cadc
barotdiv <divbarot15@gmail.com>
2026-07-21 12:46:47 +0530

    create Netflix home page with hero section, movie slider, and authentication redirect

--- frontend/src/pages/Netflix.jsx
+++ frontend/src/pages/Netflix.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Model from "../components/Model";
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import backgroundImage from "../assets/home.jpg";
@@ -14,64 +15,72 @@ import { AiOutlineInfoCircle } from "react-icons/ai";
 import Slider from "../components/Slider";
 
 function Netflix() {
-    const [isScrolled, setIsScrolled] = useState(false);
-    const movies = useSelector((state) => state.netflix.movies);
-    const genres = useSelector((state) => state.netflix.genres);
-    const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const movies = useSelector((state) => state.netflix.movies);
+  const genres = useSelector((state) => state.netflix.genres);
+  const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
 
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getGenres());
-    }, []);
+  useEffect(() => {
+    dispatch(getGenres());
+  }, []);
 
-    useEffect(() => {
-        if (genresLoaded) {
-            dispatch(fetchMovies({ genres, type: "all" }));
-        }
-    }, [genresLoaded]);
+  useEffect(() => {
+    if (genresLoaded) {
+      dispatch(fetchMovies({ genres, type: "all" }));
+    }
+  }, [genresLoaded]);
 
-    onAuthStateChanged(firebaseAuth, (currentUser) => {
-        if (!currentUser) navigate("/login");
-    });
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (!currentUser) navigate("/login");
+  });
 
-    window.onscroll = () => {
-        setIsScrolled(window.pageYOffset === 0 ? false : true);
-        return () => (window.onscroll = null);
-    };
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
 
-    return (
-        <Container>
-            <Navbar isScrolled={isScrolled} />
-            <div className="hero">
-                <img
-                    src={backgroundImage}
-                    alt="background"
-                    className="background-image"
-                />
-                <div className="container">
-                    <div className="logo">
-                        <img src={MovieLogo} alt="Movie Logo" />
-                    </div>
-                    <div className="buttons flex">
-                        <button
-                            onClick={() => navigate("/player")}
-                            className="flex j-center a-center"
-                        >
-                            <FaPlay />
-                            Play
-                        </button>
-                        <button className="flex j-center a-center">
-                            <AiOutlineInfoCircle />
-                            More Info
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <Slider movies={movies} />
-        </Container>
-    );
+  return (
+    <Container>
+      <Navbar isScrolled={isScrolled} />
+      <div className="hero">
+        <img
+          src={backgroundImage}
+          alt="background"
+          className="background-image"
+        />
+        <div className="container">
+          <div className="logo">
+            <img src={MovieLogo} alt="Movie Logo" />
+          </div>
+          <div className="buttons flex">
+            <button
+              onClick={() => navigate(`/player/${movies[0]?.id}`)}
+              className="flex j-center a-center"
+            >
+              <FaPlay />
+              Play
+            </button>
+            <button className="flex j-center a-center"
+              onClick={() => setShowModal(true)}
+            >
+              <AiOutlineInfoCircle />
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
+      <Slider movies={movies} />
+      <Model
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        movieId={movies[0]?.id}
+      />
+    </Container>
+  );
 }
 
 const Container = styled.div`
