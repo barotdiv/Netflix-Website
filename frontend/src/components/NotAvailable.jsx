ae2a06691038db6550bce6e20883922c26bb9f15
barotdiv <divbarot15@gmail.com>
2026-07-29 16:05:25 +0530

    refactor: remove unused codebase files and associated references

--- frontend/src/components/NotAvailable.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function NotAvilable() {
-    return (
-        <h1 className="not-available">
-            No Movies available for the selected genre. Please select a different genre.
-        </h1>
-    );
-}
